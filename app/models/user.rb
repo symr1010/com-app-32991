22ae0be4365517ac67ba032421cdddf373f9f940
symr1010 <bgzlud@gmail.com>
2021-01-30 00:56:18 +0900

    ユーザー認証機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do 
+    validates :nickname
+    validates :profession
+  end  
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :age_id
+    validates :sex_id
+  end
+
+  
+  belongs_to :age
+  belongs_to :sex
 end
