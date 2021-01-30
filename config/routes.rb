3032fce92dfd8d6f03a6374bd4df902a4b66f555
symr1010 <bgzlud@gmail.com>
2021-01-30 13:20:27 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :guides, only: [:index]
   root to: "guides#index"
-  # resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update]
 end
