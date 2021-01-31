9c8955f00c1d214546a6513e7064f4087eacc03c
symr1010 <bgzlud@gmail.com>
2021-01-31 20:51:24 +0900

    アクションとルーティングを設定

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,11 @@
 class GuidesController < ApplicationController
   def index
   end
+
+  def new
+  end
+  
+  def create
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :guides, only: [:index]
+  resources :guides, only: [:index, :new, :create]
   root to: "guides#index"
   resources :users, only: [:edit, :update]
 end
